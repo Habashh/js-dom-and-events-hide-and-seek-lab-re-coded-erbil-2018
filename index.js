877f8e5ca1a68d9571aeecd1919077d0fed59f9f
zaid85j <zaid85j@gmail.com>
2018-12-07 10:26:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function deepestChild(){
+  return node=document.querySelector(`#grand-node`)
+}
