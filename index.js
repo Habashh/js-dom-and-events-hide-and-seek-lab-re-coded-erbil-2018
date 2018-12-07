9526a059a5c8dba1b907204db300ddf4b083a2ad
zaid85j <zaid85j@gmail.com>
2018-12-07 11:16:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function deepestChild(){
+   node=document.querySelector("#grand-node")[3]
+   return node
+}
+function increaseRankBy(n){
+  
+}
